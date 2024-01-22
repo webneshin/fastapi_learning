cecda45b6b55084d53ab8f8c289b6242c48a8511
webneshin <sajjad.eb@gmail.com>
2024-01-22 23:48:30 +0330

    add app and parameters and request body

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,3 +14,42 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+# app ##################################################################################################################
+@app.get("/01_app/get_method")
+async def get_method():
+    return "this is 'get' method"
+
+
+@app.post("/01_app/post_method")
+async def post_method():
+    return "this is 'post' method"
+
+
+@app.put("/01_app/put_method")
+async def put_method():
+    return "this is 'put' method"
+
+
+# parameters ###########################################################################################################
+@app.get("/02_parameters/path_parameters/{name}/{age}")
+async def path_parameter(name: str, age: int):
+    return {"message": f"{name} is {age} years old.."}
+
+
+@app.get("/02_parameters/query_parameters/{name}")
+async def path_parameter(name: str, age: int = 38):
+    return {"message": f"{name} is {age} years old."}
+
+
+# request body #########################################################################################################
+class Person(BaseModel):
+    name: str
+    age: int
+    height: str | None = None
+
+
+@app.post("/03_request_body/user")
+def request_body(person: Person):
+    return person.name
